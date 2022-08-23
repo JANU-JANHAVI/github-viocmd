abbc323c1c33230ec0f44b6ddff17bd9271e763a
JANU-JANHAVI <84670122+JANU-JANHAVI@users.noreply.github.com>
2022-08-23 06:39:21 +0530

    Update product.component.ts

--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ProductService } from '../service/product.service';
-import { Product } from './product';
+import { Product } from '../module/product';
 import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
